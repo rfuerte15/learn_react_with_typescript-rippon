4462d423ee15b6b3f061fcc9c936c88f8c34db00
Rafael Fuerte-Luna <rfuerte15@gmail.com>
2025-11-02 20:53:49 -0600

    Ch. 1 updates

--- learn_react_with_typescript/src/components/alert/alert.tsx
+++ learn_react_with_typescript/src/components/alert/alert.tsx
@@ -1,5 +1,56 @@
-function Alert(){
-    return<></>;
+import "./alert.css";
+import { useState, type ReactNode } from "react";
+
+type AlertProps = {
+  type: string;
+  heading: string;
+  closable?: boolean;
+  children: ReactNode;
+  onClose?: () => void;
+};
+
+function Alert({
+  type,
+  heading,
+  closable,
+  children,
+  onClose = () => console.log("onClose triggered"),
+}: AlertProps) {
+  const [visibility, setVisibility] = useState(true);
+  const handleCloseClick = () => {
+    setVisibility(false);
+    if (onClose) {
+      onClose();
+    }
+  };
+
+  if (!visibility) {
+    return null;
+  }
+
+  console.log(`closable: ${closable}`);
+  return (
+    <div>
+      <div>
+        <span
+          role="img"
+          aria-label={type === "warning" ? "Warning" : "Information"}
+        >
+          {type === "Warning" ? "⚠️" : "ℹ️"}
+        </span>
+        <span>{heading}</span>
+      </div>
+      {closable && (
+        <button aria-label="close" onClick={handleCloseClick}>
+          <span role="img" aria-label="close">
+            ❌
+          </span>
+        </button>
+      )}
+
+      <div>{children}</div>
+    </div>
+  );
 }
 
 export default Alert;
